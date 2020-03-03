47485b555df0780a1eec372f5986e147d121b12a
Sirichai <sirichai.b@23perspective.com>
2020-03-03 18:47:59 +0700

    [ add ] clear test database in setup phrase

--- RealmSwiftTutorialTests/RealmSwiftTutorialTests.swift
+++ RealmSwiftTutorialTests/RealmSwiftTutorialTests.swift
@@ -13,6 +13,7 @@ class RealmSwiftTutorialTests: XCTestCase {
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        RealmDatabase.shareInstance.clear()
     }
 
     override func tearDown() {
